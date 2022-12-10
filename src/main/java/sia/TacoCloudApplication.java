716aa05297fc9b7e9e814e12c435280bc12b2020
tafadzwabmotsi <tafadzwabrianmotsicoding@gmail.com>
2022-12-10 20:18:11 +0200

    Initiated the taco cloud application

---
+++ src/main/java/sia/TacoCloudApplication.java
@@ -0,0 +1,14 @@
+package sia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudApplication.class, args);
+    }
+
+}
