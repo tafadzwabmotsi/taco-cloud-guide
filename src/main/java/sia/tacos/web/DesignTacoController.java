a53d187d1d193efdde3b9736afb197c1dca2ca66
tafadzwabmotsi <tafadzwabrianmotsicoding@gmail.com>
2022-12-11 11:25:11 +0200

    Added the taco design form

--- src/main/java/sia/tacos/web/DesignTacoController.java
+++ src/main/java/sia/tacos/web/DesignTacoController.java
@@ -32,7 +32,7 @@ public class DesignTacoController {
     public void addIngredientsToModel(Model model){
 
         /**
-         * Initialize all possible taco ingredients
+         * Get all possible taco ingredients
          * */
         List<Ingredient> ingredients = Arrays.asList(
                 new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
@@ -46,22 +46,44 @@ public class DesignTacoController {
                 new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
 
+
+        /**
+         * Get all the taco ingredient types and add model attribute for each
+         * given type
+         * */
         Type[] types = Ingredient.Type.values();
         for (Type type: types){
             model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
         }
     }
 
+    /**
+     * order - creates a new TacoOrder object that holds state for the order
+     * being built as the user creates tacos across multiple requests
+     * @return a new TacoOrder object
+     * */
     @ModelAttribute(name = "tacoOrder")
     public TacoOrder order(){
         return new TacoOrder();
     }
 
+    /**
+     * taco - creates a new Taco object that will be placed into the
+     * model so that the view rendered will have a non-null object to display
+     * @return a new Taco object
+     * */
     @ModelAttribute(name = "taco")
     public Taco taco(){
         return new Taco();
     }
 
+    /**
+     * showDesignForm - method that handles the GET request from "/design"
+     * It populates the given model with an empty Taco object under a key
+     * whose name is "design"
+     * @return the logical name of the view that will be used to render model
+     * to the browser
+     * */
     @GetMapping
     public String showDesignForm(){
         return "design";
